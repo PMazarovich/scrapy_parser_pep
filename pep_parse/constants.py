46b81fbbea739a8b4717cf0ba38859da3ca18c02
Peter Mazarovich <petrmazarovich@gmail.com>
2025-05-31 10:17:07 +0300

    tests

--- pep_parse/constants.py
+++ pep_parse/constants.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-RESULTS_FOLDER_NAME = "results"
 CURRENT_DATE = f"{datetime.now():%Y-%m-%d_%H-%M-%S}"
 PEP_SUMMARY_FILE = f"status_summary_{CURRENT_DATE}.csv"
 PEP_RESULTS_FILE = f"pep_{CURRENT_DATE}.csv"
+RESULTS_FOLDER_NAME = "results"
--- pep_parse/settings.py
+++ pep_parse/settings.py
@@ -6,7 +6,6 @@ BOT_NAME = "pep_parse"
 LOG_LEVEL = 'INFO'
 
 os.makedirs(RESULTS_FOLDER_NAME, exist_ok=True)
-
 FEEDS = {
     f"{RESULTS_FOLDER_NAME}/pep_%(time)s.csv": {
         "format": "csv",
